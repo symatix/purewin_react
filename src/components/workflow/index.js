8636901b0abfa99ca85a26cfb55f7170aaea5c73
Dino Kraljeta <pockshocks@gmail.com>
2018-01-07 14:22:38 +0100

    init heroku deploy

--- src/components/workflow/index.js
+++ src/components/workflow/index.js
@@ -1,36 +1,48 @@
 import React from 'react';
-import Grid from 'material-ui/Grid';
 import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
-import Paper from 'material-ui/Paper';
+import {withStyles} from 'material-ui/styles';
+import ExpansionPanel, {ExpansionPanelSummary, ExpansionPanelDetails} from 'material-ui/ExpansionPanel';
+import Graph from './Graph';
 import Typography from 'material-ui/Typography';
-
+import ExpandMoreIcon from 'material-ui-icons/ExpandMore';
 
 const styles = theme => ({
-  root: theme.mixins.gutters({
-    paddingTop: 16,
-    paddingBottom: 16,
-    marginTop: theme.spacing.unit * 3,
-  }),
+    root: {
+        marginTop: theme.spacing.unit * 3,
+        width: '100%'
+    },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular
+    }
 });
 
-const Workflow = (props) => {
-  const { classes } = props;
-  return (
-      <Grid container justify="center" direction="row">  
-        <Grid item xs={11} md={11} lg={11}>
-          <Paper className={classes.root} elevation={4}>
-            <Typography type="headline" component="h3">
-              Workflow
-            </Typography>
-          </Paper>
-        </Grid>
-      </Grid>
-  )
+function SimpleExpansionPanel(props) {
+    const {classes} = props;
+    return (
+        <div className={classes.root}>
+            <ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={< ExpandMoreIcon />}>
+                    <Typography type="headline" component="h3">
+                        Workflow
+                    </Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
+                    <Typography type="body1">
+                        &#8227; To add nodes, hold shift and click on the grid.<br/>
+                        &#8227; To add connections, hold shift and click/drag to between nodes.<br/>
+                        &#8227; To delete a node or connection, click on it and press delete.<br/>
+                        &#8227; Click and drag nodes to change their position.
+                    </Typography>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>
+            <Graph/>
+        </div>
+    );
 }
 
-Workflow.propTypes = {
-  classes: PropTypes.object.isRequired,
+SimpleExpansionPanel.propTypes = {
+    classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Workflow);
+export default withStyles(styles)(SimpleExpansionPanel);
